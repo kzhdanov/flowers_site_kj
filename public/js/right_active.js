77a4b49390db9aa7cf85286381504ac83c1a8c26
kj <pinchv@gmail.com>
2017-02-21 22:25:25 +0300

    add scroll event in jqwery

---
+++ public/js/right_active.js
@@ -0,0 +1,21 @@
+$(function () {
+	var position = 0;
+	var index = 1;
+	$(document).off('scroll');
+	$(document).on('scroll', function (e) {
+		var st = $(this).scrollTop();
+		if(st+800 > $('.foto_ex').position().top) {
+			if($(".foto_ex img").eq(0).css('opacity') === '0') {
+				$(".foto_ex img").eq(0).css('opacity', 1);
+			}
+		}
+   		if(st+700 > $(".foto_ex img").eq(index).position().top) {
+   			$(".foto_ex img").eq(index).css('opacity', 1);
+   			index+=1;
+   		}
+   		if(index === 5)	{
+   			$(document).off('scroll');
+   			return false;
+   		}
+	})
+});
